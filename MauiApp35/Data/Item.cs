9d2fbc3c5f5299e11c4033f26cac5ffc668b7465
Yafet-Siele <167128410+Yafet-Siele@users.noreply.github.com>
2024-10-06 21:59:19 -0600

    Add project files.

---
+++ MauiApp35/Data/Item.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiApp35.Data
+{
+    public class Item
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Equipment { get; set; }
+        public string Category { get; set; }
+        public float Cost { get; set; }
+        public string Description { get; set; }
+    }
+}
+
